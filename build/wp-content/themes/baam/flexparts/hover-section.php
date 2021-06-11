eb5a02cb87ef9752987055db1d75719b6613b213
Sative <jakub@sative.co.uk>
2021-06-11 11:22:24 +0100

    asdasdsad

--- build/wp-content/themes/baam/flexparts/hover-section.php
+++ build/wp-content/themes/baam/flexparts/hover-section.php
@@ -1,7 +1,7 @@
 <section class="cta-big pb-4">
 	<div class="container-fluid px-0">
 		<div class="row align-content-stretch">
-			<div class="col-lg-5 pl-0 pr-2 card-reveal">
+			<div class="col-lg-5 pl-0 pr-2 card-reveal hover-effect-section">
 				<div class="h-100 card border-0 border-radius bg-grey7">
 					<div class="card-body p-4 h-100 d-flex flex-wrap align-items-center">
 						<div class="w-100 text-left">
@@ -21,7 +21,7 @@
 					<a href="<?php echo get_sub_field('link')['url']; ?>" class="whole-element-link"></a>
 				<?php endif; ?>
 			</div>
-			<div class="col-lg-7 pr-0">
+			<div class="col-lg-7 pr-0 hover-effect-section">
 				<div class="h-100 card border-0 border-radius bg-grey2 p-0 overflow-hidden">
 					<img data-src="<?php echo get_sub_field('image')['url']; ?>" alt="<?php echo get_sub_field('image')['url']; ?>" class="lazy bg-cover scale">
 				</div>
